1e242798126349209f39dd09a7053926c0f00819
amarin <17020181+asiripanich@users.noreply.github.com>
2021-07-25 00:59:53 +1000

    Fixes and run R-CMD-CHECK on GitHub CI (#6)

--- R/onnx-package.R
+++ R/onnx-package.R
@@ -1,7 +1,10 @@
-#' R Interface to ONNX
-#' 
-#' @docType package
-#' @name onnx
+#' @keywords internal
+"_PACKAGE"
+
+# The following block is used by usethis to automatically manage
+# roxygen namespace tags. Modify with care!
+## usethis namespace: start
+## usethis namespace: end
 NULL
 
 onnx <<- NULL
